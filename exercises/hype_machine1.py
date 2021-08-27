ae31243b627184b16d944f39f67835ac1257a11e
Ella <ellapark@live.unc.edu>
2021-08-27 18:40:38 -0400

    correct file hype machine

---
+++ exercises/hype_machine1.py
@@ -0,0 +1,8 @@
+"""Hype Machine."""
+
+__author__ = "730330121"
+
+name: str = input("What is your name? ")
+print("Wow, you're the bomb dot com, " + str(name) + ". ")
+print(str(name) + " , you're gorge, obvi! ")
+print("Lastly, " + str(name) + " you're totally radical! ")
