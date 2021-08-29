6ac51509268065d51ff21503f23778b554863968
Ella <ellapark@live.unc.edu>
2021-08-29 19:50:25 -0400

    final edits on ex01

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -2,7 +2,7 @@
 
 __author__ = "730330121"
 
-name: str = input("What is your name? ")
-print("Wow, you're the bomb dot com, " + str(name) + ". ")
-print(str(name) + " , you're gorge, obvi! ")
-print("Lastly, " + str(name) + " you're totally radical! ")
+your_name: str = input("What is your name? ")
+print("Wow, you're the bomb dot com, " + str(your_name) + ". ")
+print(str(your_name) + " , you're gorge, obvi! ")
+print("Lastly, " + str(your_name) + " you're totally radical! ")
--- exercises/ex01/numeric_operators.py
+++ exercises/ex01/numeric_operators.py
@@ -2,12 +2,12 @@
 
 __author__ = "730330121"
 
-left: int = int(input("What is the left number? "))
-right: int = int(input("What is the right number? "))
-print("Left-hand side: " + str(left))
-print("Right-hand side: " + str(right))
+left_value: int = int(input("What is the left number? "))
+right_value: int = int(input("What is the right number? "))
+print("Left-hand side: " + str(left_value))
+print("Right-hand side: " + str(right_value))
 
-print(str(left) + " ** " + str(right) + " is " + str(left ** right))
-print(str(left) + " / " + str(right) + " is " + str(left / right))
-print(str(left) + " // " + str(right) + " is " + str(left // right))
-print(str(left) + " % " + str(right) + " is " + str(left % right))
+print(str(left_value) + " ** " + str(right_value) + " is " + str(left_value ** right_value))
+print(str(left_value) + " / " + str(right_value) + " is " + str(left_value / right_value))
+print(str(left_value) + " // " + str(right_value) + " is " + str(left_value // right_value))
+print(str(left_value) + " % " + str(right_value) + " is " + str(left_value % right_value))
