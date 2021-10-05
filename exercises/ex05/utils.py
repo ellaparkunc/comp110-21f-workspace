85fbfb899413d19abbb09c9c74e5303c47330b84
Ella <ellapark@live.unc.edu>
2021-10-04 23:16:46 -0400

    half way through ex05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,30 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730330121"
 
 # Define your functions below
+
+
+def only_evens(xs: list[int]) -> list[int]:
+    """Given a list will spit out the list of only integers aka will ask is odd? then that number."""
+    i: int = 0
+    while i < len(xs):
+        if xs[i] % 2 != 0:
+            xs.pop(i)
+        else:
+            i += 1
+    return(xs)
+
+
+def sub(xs: list[int], start: int, end: int) -> list[int]:
+    new: list[int] = []
+    i: int = start
+    x: int = xs[i]
+    while start < end:
+        new.append(x)
+        start += 1
+    return(new)
+
+
+if __name__ == "__main__":
+    only_evens([1, 2, 3, 4])
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -4,3 +4,32 @@
 # from exercises.ex05.utils import only_evens, sub, concat
 
 __author__ = "123456789"
+
+from exercises.ex05.utils import only_evens
+
+
+def test_only_evens_empty() -> None:
+    """Tests for if the argument is empty."""
+    xs: list[int] = []
+    assert only_evens(xs) == []
+
+
+def test_all_odds() -> None:
+    """Tests for if the function is given all odd numbers."""
+    xs: list[int] = [1, 3, 5]
+    assert only_evens(xs) == []
+
+
+def test_all_evens() -> None:
+    """Tests to see if the function will leave a list alone which is already all even."""
+    xs: list[int] = [2, 4, 6, 8]
+    assert only_evens(xs) == [2, 4, 6, 8]
+
+
+from exercises.ex05.utils import sub
+
+
+def test_sub() -> None:
+    """Tests sub."""
+    example: list[int] = [10, 20, 30, 40]
+    assert sub(example, 1, 3) == [20, 30]
